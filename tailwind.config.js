9ce054c38e3f3cf857c8fc256018e05b7a062a2a
Sophia Edeki <sophiesse143@gmail.com>
2023-01-04 14:56:21 +0100

    added flowbite pludin

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,14 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite-typography'),
-    // ...
-  ],
+    
+    require('flowbite/plugin')
+]
+
 }
